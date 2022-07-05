27217b0a01a4b1a13f4dad2eac977e3c6fe054a6
ingridflack <ingrid.flack@newtail.com.br>
2022-07-04 21:35:39 -0300

    Styles config and routes added

---
+++ src/pages/_app.tsx
@@ -0,0 +1,17 @@
+import "../styles/global.ts";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+import { GlobalStyle } from "../styles/global";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ src/styles/theme.ts
@@ -0,0 +1,24 @@
+const theme = {
+  colors: {
+    background: "#000",
+
+    gray: {
+      platinum: "#e1e1e6",
+      silver: "#a8a8b3",
+      dark: "#141414",
+    },
+
+    primary: "#e50914",
+    secondary: "#b20710",
+  },
+  fontSize: {
+    small: "0.75rem",
+    medium: "0.875rem",
+    normal: "1rem",
+    large: "1.5rem",
+  },
+} as const;
+
+export type Theme = typeof theme;
+
+export default theme;
