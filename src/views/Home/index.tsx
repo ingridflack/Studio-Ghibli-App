35981a9b71da28150458ad58750e7bf2469b2a76
ingridflack <ingrid.flack@newtail.com.br>
2022-07-06 14:29:19 -0300

    Add search bar on home page

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,18 +1,55 @@
+import { ChangeEvent, useMemo, useState } from "react";
+
 import MovieCard from "../../components/MovieCard";
+import Header from "../../components/Header";
 import { IMovie } from "../../types";
 
 import * as S from "./styles";
+import Alert from "../../components/Alert";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MainContainer } from "../../styles/shared";
 interface HomeProps {
   movies: IMovie[];
 }
 
 const Home = ({ movies }: HomeProps) => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    const search = e.target.value;
+
+    setSearchValue(search);
+  };
+
+  const filteredMovies = useMemo(
+    () =>
+      movies.filter(({ title }) =>
+        title.toLowerCase().includes(searchValue.toLowerCase())
+      ),
+    [movies, searchValue]
+  );
+
   return (
-    <S.MoviesList>
-      {movies.map((movie) => (
-        <MovieCard key={movie.id} movie={movie} />
-      ))}
-    </S.MoviesList>
+    <MainContainer>
+      <Header />
+
+      <form>
+        <S.Input
+          placeholder="Search"
+          value={searchValue}
+          onChange={handleSearch}
+        />
+        <AiOutlineSearch />
+      </form>
+
+      <S.MoviesList>
+        {filteredMovies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
+        ))}
+
+        {!filteredMovies.length && <Alert>No results found</Alert>}
+      </S.MoviesList>
+    </MainContainer>
   );
 };
 
